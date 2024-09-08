d196d471fc234436c2cb920c8af62a34b1436a33
Stupendos <topakovdenis671@gmail.com>
2024-09-08 12:27:36 +0500

    ...

---
+++ src/test/java/com/stupendos/calculatordemo/CalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stupendos.calculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
